b7c302a22c4885ee5442b47d20ddb0e5fd0f7d00
Vitalik <mr.iaremko@mail.ru>
2019-03-25 18:51:58 +0200

    Create Project

---
+++ BlogAPI/BlogAPI/Infrastructure/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Infrastructure.DependencyInjection
+{
+    public static class DependecyInjectionExtensions
+    {
+        public static void AddModule<T>(this IServiceCollection services) where T : class, IDependencyModule, new()
+        {
+            new T().ConfigureServices(services);
+        }
+    }
+}
